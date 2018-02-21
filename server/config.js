cfa7c14852864cf9c20c82b692960154eb43f6e2
Quang_Nguyen <quangnguyen@goappable.com>
2018-02-21 11:46:06 +0700

    Fix connection string

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 const config = {
-  mongoURL: process.env.MONGO_URL || 'mongodb+srv://admin:admin@cluster0-svsgx.mongodb.net/mern-starter',
+  mongoURL: process.env.MONGO_URL || 'mongodb://admin:admin@cluster0-shard-00-00-svsgx.mongodb.net:27017,cluster0-shard-00-01-svsgx.mongodb.net:27017,cluster0-shard-00-02-svsgx.mongodb.net:27017/mern-starter?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin',
   port: process.env.PORT || 8000,
 };
 
