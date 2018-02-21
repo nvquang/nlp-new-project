6eb6183c41dee6919ed052743ba9e1c7391ecde6
Quang_Nguyen <quangnguyen@goappable.com>
2018-02-21 10:16:23 +0700

    initial commit

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 const config = {
-  mongoURL: process.env.MONGO_URL || 'mongodb://localhost:27017/mern-starter',
+  mongoURL: process.env.MONGO_URL || 'mongodb+srv://admin:admin@cluster0-svsgx.mongodb.net/mern-starter',
   port: process.env.PORT || 8000,
 };
 
